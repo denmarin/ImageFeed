52dba313c8b76ace27f8ef31a5d7475148932b85
denmarin <Danya6846084@gmail.com>
2025-10-11 15:46:49 +0400

    no message

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Yury Semenyushkin on 11.10.25.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+}
